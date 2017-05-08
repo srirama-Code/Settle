7d51e7962f7554bb775e14492bf8b7277bff18ab
sriram-Code <sriramazure@gmail.com>
2017-05-08 17:54:45 -0500

    gulp issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
-var sass = require('gulp-saas');
+var sass = require('gulp-sass');
 
-gulp.task('saas', function()
+gulp.task('sass', function()
 {
 gulp.src('*.scss')
 .pipe(sass())
